a2e93ddafb5686d4831f4acb2f4fd81bba502aa0
yunlulu <905752387@qq.com>
2016-12-13 16:57:01 +0800

    1213 16:56

--- handlebars/js/global.js
+++ handlebars/js/global.js
@@ -1,38 +1,38 @@
-// jquery handlebars方法
-
-(function($) {
+// jquery扩展插件----handlebars
+  (function($) {
     var compiled = {};
     $.fn.handlebars = function(template, data) {
         if (template instanceof jQuery) {
             template = $(template).html();
         }
-            console.log($(template));
-            console.log(template);
     compiled[template] = Handlebars.compile(template);
     this.html(compiled[template](data));
-
     };
 })(jQuery);
 
 
 // 
 var datas = {
+    data2:{name:"22222222"},
     data1:[
         {
          name: "张三",
          sex: "男",
-         age: 30 
+         age: 30,
+         fav:['apple1111','banna','orange']
          },
          {
          name: "李四",
          sex: "男",
-         age: 23
+         age: 23,
+           fav:['apple','banna22222222222','orange']
        },{
          name: "甜妞",
          sex: "女",
-        age: 18
+        age: 18,
+          fav:['apple','banna','orange333333']
        }
        ]
 
    };
-$(".box1").handlebars("#temp1",datas);
+$(".box1").handlebars($("#temp1"),datas);
